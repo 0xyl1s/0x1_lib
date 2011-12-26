3ad3cf4cca90e0f4b2acd0da6a33024438e162f5
epima <epima@3eclipses.com>
2011-12-26 21:44:00 +0100

    standard lib extension and refactoring

--- ec1/lib/toolkit/standard.rb
+++ ec1/lib/toolkit/standard.rb
@@ -11,28 +11,36 @@ module Ec1 module Lib module Toolkit module Standard
     Time.new.strftime("%F_%H:%M")
   end
   alias :ec1__time :e__datetime
+
   def e__datetime_sec
     Time.new.strftime("%F_%H:%M:%S")
   end
 
-  def ec1__dir_ls(directory_raw, filter_raw = '*')
-    raise "#{directory_raw} is not a directory" unless File.directory?(directory_raw)
+  def e__dir_ls(directory_raw, filter_raw = '*')
+    abort "#{directory_raw} is not a directory" unless e__is_a_dir?(directory_raw)
     # making sure directory path end with /
     directory = directory_raw << ('/') unless directory_raw.match(/\/\Z/)
     Dir["#{directory}#{filter_raw}"]
   end
-  def ec1__dir_current
+  alias :ec1__dir_ls :e__dir_ls
+
+  def e__dir_current
     puts Dir.pwd
   end
-  def ec1__file_read(file)
-    raise "Can't read #{file}" unless File.readable?(file)
+  alias :ec1__dir_current :e__dir_current
+
+  def e__file_read(file)
+    abort "Can't read #{file}" unless e__file_readable?(file)
     File.read(file)
   end
+  alias :ec1__file_read :e__file_read
+
   def e__string_contain_only_numbers?(string)
     abort "checked string-number must be a string..." unless string.is_a?(String)
     string =~ /^[\d]+$/ ? false : true
   end
-  def ec1__random_name(number_of_characters=13)
+
+  def e__random_name(number_of_characters=13)
     letters_lower = ('a'..'z').to_a
     letters_upper = ('A'..'Z').to_a
     numbers = (0..9).to_a.collect {|i| i.to_s}
@@ -43,16 +51,22 @@ module Ec1 module Lib module Toolkit module Standard
     end
     e_random_name
   end
+  alias :ec1__random_name :e__random_name
+
   #gets a file argument [string], and returns [array] of lines [string]
-  def ec1__file_readlines(file)
-    raise "Can't read #{file}" unless File.readable?(file)
+  def e__file_readlines(file)
+    abort "Can't read #{file}" unless e__file_readable?(file)
     File.readlines(file)
   end
+  alias :ec1__file_readlines :e__file_readlines
+
   #gets a file argument [string], and returns [array] of lines [string], excluding comments
-  def ec1__file_readlines_minus_comments(file, comment_character = '#')
-    ec1__file_readlines(file)
+  def e__file_readlines_minus_comments(file, comment_character = '#')
+    e__file_readlines(file)
   end
-  def ec1__confirm(message='y(es) or no? ')
+  alias :ec1__file_readlines_minus_comments :e__file_readlines_minus_comments
+
+  def e__confirm(message='y(es) or no? ')
     print message
     e_confirm = gets.chomp
     case e_confirm
@@ -62,45 +76,120 @@ module Ec1 module Lib module Toolkit module Standard
       false
     end
   end
-  def ec1__file_save_unsecured(e_file_content, e_file_name, e_file_mode='600')
+  alias :ec1__confirm :e__confirm
+
+  def e__file_save_unsecured(e_file_content, e_file_name, e_file_mode='600')
     File.open(e_file_name, 'w') {|f| f.write(e_file_content) }
     File.chmod(e_file_mode.to_i(8), e_file_name)
   end
-  def ec1__file_save_nl(e_file_content_raw, e_file_name, e_file_mode_raw=600)
+  alias :ec1__file_save_unsecured :e__file_save_unsecured
+
+  def e__file_save_nl(e_file_content_raw, e_file_name, e_file_mode_raw=600)
     e_file_content = "#{e_file_content_raw}\n"
-    ec1__file_save(e_file_content, e_file_name, e_file_mode_raw)
+    e__file_save(e_file_content, e_file_name, e_file_mode_raw)
   end
-  def ec1__file_save(e_file_content, e_file_name, e_file_mode_raw=600)
-    require 'fileutils'
+  alias :ec1__file_save_nl :e__file_save_nl
+
+  def ec1__file_save(e_file_content, e_filename, e_file_mode_raw=600)
+    e_tempfile = "#{e_file_name}.ec1temp.#{e__random_name}"
+    abort "file #{e_filename} exists already" if e__is_a_file?(e_filename)
+    abort "can't write file #{e_tempfile}" unless e__file_write(e_tempfile, e_file_content)
+    abort "can't set file #{e_tempfile} mode to #{e_file_mode}" unless e__file_chmod(e_tempfile, e_file_mode)
+    abort "can't overwrite #{e_tempfile} to #{e_filename}" unless e__file_move(e_tempfile, e_filename)
+  end
+
+  def e__file_overwrite(e_file_content, e_file_name)
+    e_tempfile = "#{e_file_name}.ec1temp.#{e__random_name}"
+    abort "can't write file #{e_tempfile}" unless e__file_write(e_tempfile, e_file_content)
+    abort "can't overwrite #{e_tempfile} to #{e_file_name}" unless e__file_move(e_tempfile, e_file_name)
+  end
+
+  def e__file_write(content, filename)
+    File.open(filename, 'w') {|f| f.write(e_file_content) }
+  end
+
+  def e__file_write_binary(content, filename)
+    File.open(filename, 'wb') {|f| f.write(e_file_content) }
+  end
+
+  def e__file_puts(content, filename)
+    File.open(filename, 'w') {|f| f.puts(e_file_content) }
+  end
+
+  def e__file_chmod(e_file_name, e_file_mode_raw=600)
     e_file_mode = "0#{e_file_mode_raw.to_s}".to_i(8)
-    raise "file #{e_file_name} exists already" if File.exist?(e_file_name) and not ec1__confirm("confirm overwriting #{e_file_name}? ")
-    e_tempfile = "#{e_file_name}.ec1temp.#{ec1__random_name}"
-    raise "can't write file #{e_tempfile}" unless File.open(e_tempfile, 'w') {|f| f.write(e_file_content) }
-    raise "can't set file #{e_tempfile} mode to #{e_file_mode}" unless File.chmod(e_file_mode, e_tempfile)
-    FileUtils.mv(e_tempfile, e_file_name)
+    File.chmod(e_file_mode, e_tempfile)
   end
-  def ec1__dir_copy(sourcedirectory, destination_uri)
-    raise "Can't access directory: #{sourcedirectory}" unless File.directory?(sourcedirectory)
-    raise "destination_uri already exists: #{destination_uri}" if File.directory?(destination_uri)
+
+  def e__file_move(initial_filename, new_filename)
+    require 'fileutils'
+    FileUtils.mv(initial_filename, new_filename)
+  end
+
+  def e__dir_copy(sourcedirectory, destination_uri)
+    abort "Can't access directory: #{sourcedirectory}" unless e__is_a_dir?(sourcedirectory)
     destinationdirectory = File.dirname(destination_uri)
-    ec1__directory_writable(destinationdirectory)
-    FileUtils.cp_r sourcedirectory, destination_uri
+    abort "destination_uri already exists: #{destinationdirectory}" if e__is_a_dir?(destinationdirectory)
+    abort "directory #{destinationdirectory} isn't writable" unless e__dir_writable?(destinationdirectory)
+    FileUtils.cp_r sourcedirectory, destinationdirectory
   end
-  def ec1__filename(fullpath)
-    Pathname.new(fullpath).path
+  alias :ec1__dir_copy :e__dir_copy
+
+  def e__is_a_file?(file)
+    File.exists?(file) ? true : false
   end
+
+  def e__is_a_symlink?(file)
+    File.symlink?(file) ? true : false
+  end
+
+  def e__is_a_dir?(directory)
+    File.directory?(directory) ? true : false
+  end
+
+  def e__file_readable?(file)
+    File.readable?(file) ? true : false
+  end
+
+  def e__file_writable?(file)
+    File.writable?(file) ? true : false
+  end
+
+  def e__dir_readable?(directory)
+    abort if e__is_a_dir?(directory)
+    File.readable?(directory) ? true : false
+  end
+
+  def e__dir_writable?(directory)
+    abort if e__is_a_dir?(directory)
+    File.writable?(directory) ? true : false
+  end
+
   def ec1__
     puts "epiculture α --"
   end
-  def ec1__check_file_exist(filename)
+
+
+#_______________________________________________________________________
+################## ☣ DEPRECATED ☣ ######################################
+#☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣☣
+
+  def ec1__filename(fullpath)
+    Pathname.new(fullpath).path
+  end
+
+  def e__check_file_exist(filename)
     File.exist?(filename)
   end
+
   def ec1__directory_writable(directory)
     raise "directory #{directory} isn't writable" unless File.writable?(directory)
   end
+
   def ec1__new_file_writable(filename)
     raise "file #{filename} doesn't exist or is inaccessible" unless File.exist?(filename)
   end
+
 end end end end
 
 
