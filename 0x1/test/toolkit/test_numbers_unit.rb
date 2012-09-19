83cd9713991f5708e3c86182b1d222e62d085b17
pima <pima@3eclipses.com>
2012-09-19 14:46:13 +0200

    test3

--- 0x1/test/toolkit/test_numbers_unit.rb
+++ 0x1/test/toolkit/test_numbers_unit.rb
@@ -15,7 +15,7 @@ class Test0x1LibNumbers < MiniTest::Unit::TestCase
   def test_x__is_an_integer?
     assert(x__is_an_integer?(2))
     refute(x__is_an_integer?('a'))
-    #2assert(x__is_an_integer?('i'), "letter i should not be detected as an integer")
+    #3assert(x__is_an_integer?('i'), "letter i should not be detected as an integer")
     assert(x__is_an_integer?('i'))
   end
 
