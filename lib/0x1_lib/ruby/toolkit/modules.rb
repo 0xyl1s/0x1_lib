eb38f4ddf1669ec71757cd30c7bf01e44b8e92ca
pima <pima@3eclipses.com>
2012-10-03 21:14:49 +0200

    segmented 0x1_lib by language

--- lib/0x1_lib/ruby/toolkit/modules.rb
+++ lib/0x1_lib/ruby/toolkit/modules.rb
@@ -11,10 +11,10 @@ module X module Lib module Toolkit module ModulesClassesMethods
     x_lib_modules2load.each do |module2load|
       case module2load
       when :standard
-        require "#{x_lib_path_base}/lib/0x1_lib/toolkit/standard.rb"
+        require "#{x_lib_path_base}/lib/0x1_lib/ruby/toolkit/standard.rb"
         extend X::Lib::Toolkit::Standard
       when :online
-        require "#{x_lib_path_base}/lib/0x1_lib/toolkit/online.rb"
+        require "#{x_lib_path_base}/lib/0x1_lib/ruby/toolkit/online.rb"
         extend X::Lib::Toolkit::Online
       else
         abort "E: module2load: no such module as #{module2load}"
